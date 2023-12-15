10849fb6766cc7d9acbf9c1010e1d8b2a377a28f
bynneh <bynneh@gmail.com>
2023-12-15 22:19:45 +0400

    debug

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="container mx-auto max-w-5xl px-3 xs:px-6 md:px-8">
     <AppHeader />
     <main class="mt-4 md:mt-6 lg:mt-8">
-      <slot />
+      <ClientOnly> <slot /></ClientOnly>
     </main>
   </div>
 </template>
